a185be14fb498f69e5e489a9022e84f40ecb6694
desabi <desabi@live.com>
2025-04-18 20:31:35 -0600

    refactor: functional interface impl, supplier

--- functional_interfaces_impl/src/main/java/com/desabisc/supplier/SupplierExample.java
+++ functional_interfaces_impl/src/main/java/com/desabisc/supplier/SupplierExample.java
@@ -5,22 +5,22 @@ import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.function.Supplier;
 
+/**
+ * Supplier is a functional interface that is used to generate data.
+ * Takes no arguments and returns a value.
+ * method: get()
+ * */
 public class SupplierExample {
 
     private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
-        /*
-        * Supplier is a functional interface that is used to generate data.
-        * Takes no arguments and returns a value.
-        * method: get()
-        * */
 
         Supplier<Integer> integerSupplier = () -> {
             Random randomGenerator = new Random();
             return randomGenerator.nextInt(100);
         };
-        System.out.println("Supplier.get() = " + integerSupplier.get());
+        System.out.println("Integer random supplier = " + integerSupplier.get());
 
         Supplier<LocalDateTime> supplierLocalDateTime = () -> LocalDateTime.now();
         LocalDateTime localDateTime = supplierLocalDateTime.get();
